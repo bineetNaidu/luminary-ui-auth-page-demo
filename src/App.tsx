4c529ea6f2185e5950d7f894bbe97e28aa8eae6e
Bineet Naidu <bineet.s.naidu@gmail.com>
2026-07-09 18:14:04 +0530

    Add a base login form with luminary ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 const App = () => {
   return (
-    <main>
-      <h1>Auth Pages Demo</h1>
-    </main>
+    <div>
+      <form id="form" className="glass-card">
+        <div className="auth_welcome_container">
+          <img src="/logo.png" alt="Luminary Logo" />
+          <h2>Welcome Back, <span className="auth_sess_name">Thomas</span></h2>
+        </div>
+        <label htmlFor="email">Your Email</label>
+        <input id="email" type="email" placeholder="you@example.com" />
+        <label htmlFor="pass">Your Password</label>
+        <input id="pass" type="password" placeholder="Password" />
+        <button className="vibrant success glow">Login</button>
+        <p>Don't have an account?. <a href="#">Register now!</a></p>
+      </form>
+    </div>
   )
 }
 
